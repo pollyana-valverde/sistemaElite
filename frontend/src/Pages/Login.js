473efc41127eee870e0bc0494188071999f676b6
JessicaAJesus <jessicaarruda06@gmail.com>
2024-05-07 07:59:09 -0300

    login adicionado

--- frontend/src/Pages/Login.js
+++ frontend/src/Pages/Login.js
@@ -22,7 +22,7 @@ const Login = () => {
 const handleSubmit = async (e) => {
   e.preventDefault();
   try {
-    const response = await fetch('http://localhost:3001/login/cpf', {
+    const response = await fetch('http://localhost:3001/login', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
