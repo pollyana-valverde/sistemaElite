c46c4cfe0af84149f459c253656d8ca837ba83b1
bebellacarol <isabellacarolinasantos2006@gmail.com>
2024-05-16 11:13:19 -0300

    merge Isa

--- frontend/src/Pages/Produtos.js
+++ frontend/src/Pages/Produtos.js
@@ -1,2 +1,15 @@
+//Cadastro
+import React from "react";
+import ProdutosForm from "../Components/ProdutosForm";
+const Produtos = () => {
+  return (
+    <>
+      <div>
+        <h2>Cadastrar Produtos</h2>
+        <ProdutosForm />
+      </div>
+    </>
+  );
+};
 
 export default Produtos;
--- frontend/src/Routes.js
+++ frontend/src/Routes.js
@@ -10,22 +10,86 @@ import Login from "./Pages/Login";
 import Logout from "./Pages/Logout";
 import TabelaUsuarios from "./Pages/ListaUsuarios";
 import FornecerCadastro from "./Pages/FornecedorCadastro";
-import TabelaFornecedor from "./Components/TabelaFornecedor";
+import TabelaFornecedor from "./Pages/ListaFornecedores";
+import ClienteCadastro from "./Pages/CadastroClientes";
+import ListaClientes from "./Pages/ListaClientes";
+import ContasReceber from "./Pages/ContasReceber";
+import ContasPagar from "./Pages/ContasPagar";
+
+import Produtos from './Pages/Produtos';
+import ListaProdutos from "./Pages/ListaProdutos";
 
 const Rotas = () => {
-  return (
-    <>
-      <Routes>
-        <Route path="/" exact element={<Home />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/contato" element={<Contato />} />
-        <Route path="/cadastro" element={<Cadastro />} />
-        <Route path="/FornecedorCadastro" element={<FornecerCadastro />} />
-        <Route path="/listaUsuarios" element={<TabelaUsuarios />} />
-        <Route path="/ListaFornecedores" element={<TabelaFornecedor />} />
-      </Routes>
-    </>
-  );
+  const { token } = useAuth();
+
+
+  //define rotas públicas acessíveis para todos os usuários
+  const rotasPublicas = [
+    {
+      path: "/cadastro",
+      element: <Cadastro />
+    }
+  ];
+
+  //define rotas somente acessíveis para usuários logados
+  const rotasSomenteAutenticados = [
+    {
+      path: "/",
+      element: <ProtectedRoute />,
+      children: [
+        {
+          path: "/",
+          element: <Home />,
+        },{
+          path: "/ListaClientes",
+          element: <ClienteCadastro />,
+        },{
+          path: "/listaUsuarios",
+          element: <TabelaUsuarios />,
+        },{
+          path: "/logout",
+          element: <Logout />
+        }
+      ],
+    },
+  ];
+
+  //define rotas somente acessíveis para usuários sem login
+  const rotasNaoAutenticados = [
+    {
+      path: "/cadastro",
+      element: <Cadastro />,
+    },{
+      path: "/login",
+      element: <Login />
+    },
+  ];
+
+  const router = createBrowserRouter([
+    ...rotasPublicas,
+    ...(!token ? rotasNaoAutenticados : []),
+    ...rotasSomenteAutenticados,
+  ]);
+
+
+  return <RouterProvider router={router} />;
+  // (
+  //   <>
+  //     <Routes>
+        
+  //       <Route path="/" exact element={<Home />} />
+  //       <Route path="/login" element={<Login />} />
+  //       <Route path="/contato" element={<Contato />} />
+  //       <Route path="/cadastro" element={<Cadastro />} />
+  //       <Route path="/FornecedorCadastro" element={<FornecerCadastro />} />
+  //       <Route path="/listaUsuarios" element={<TabelaUsuarios />} />
+  //       <Route path="/ListaFornecedores" element={<TabelaFornecedor />} />
+  //       <Route path="/CadastroClientes" element={<ClienteCadastro />} />
+  //       <Route path="/ListaClientes" element={<ListaClientes />} />
+  //       <Route path="/ContasReceber" element={<ContasReceber />} />
+  //     </Routes>
+  //   </>
+  // );
 };
 
 export default Rotas;
