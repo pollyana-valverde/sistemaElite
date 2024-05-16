ede2186eb851d755f0947c544fd632c7d9abe767
Pollyana Valverde <valverdepolly@gmail.com>
2024-05-16 15:40:47 -0300

    Rota para funcionário

--- backend/routes.js
+++ backend/routes.js
@@ -469,6 +469,66 @@ router.delete('/produtos/:idCarro', (req, res) => {idCarro
   });
 });
 
+
+///////////////////////////////////////// cadastro do funcionário ////////////////////////////////////
+// Rota para listar todos os registros
+router.get('/funcionario', (req, res) => {
+  connection.query('SELECT * FROM funcionario', (err, results) => {
+    if (err) {
+      console.error('Erro ao buscar os registros:', err);
+      res.status(500).json({ error: 'Erro ao buscar os registros' });
+      return;
+    }
+    res.json(results);
+  });
+});
+
+// Rota para buscar um registro específico pelo ID
+router.get('/funcionario/:idFuncionario', (req, res) => {
+  const { id } = req.params;
+  connection.query('SELECT * FROM funcionario WHERE idFuncionario = ?', [id], (err, results) => {
+    if (err) {
+      console.error('Erro ao buscar o registro:', err);
+      res.status(500).json({ error: 'Erro ao buscar o registro' });
+      return;
+    }
+    if (results.length === 0) {
+      res.status(404).json({ error: 'Registro não encontrado' });
+      return;
+    }
+    res.json(results[0]);
+  });
+});
+// Rota para criar um novo registro
+router.post('/funcionario', (req, res) => {
+  const { nome, email, cpf, endereco, telefone, senha } = req.body;
+  connection.query('INSERT INTO funcionario (nome, email, cpf, endereco, telefone, senha) VALUES (?, ?, ?, ?, ?, ?)', 
+    [nome, email, cpf, endereco, telefone, senha], (err, result) => {
+    if (err) {
+      console.error('Erro ao criar o registro:', err);
+      res.status(500).json({ error: 'Erro ao criar o registro' });
+      return;
+    }
+    res.status(201).json({ message: 'Registro criado com sucesso', id: result.insertId });
+  });
+});
+
+// Rota para atualizar um registro existente pelo ID
+router.put('/funcionario/:idFuncionario', (req, res) => {
+  const { id } = req.params;
+  const { nome, email, cpf, endereco, telefone, senha } = req.body;
+  connection.query('UPDATE funcionario SET nome = ?, email = ?, cpf = ?, endereco = ?, telefone = ?, senha = ? WHERE idFuncionario = ?', 
+    [nome, email, cpf, endereco, telefone, senha, id], (err, result) => {
+    if (err) {
+      console.error('Erro ao atualizar o registro:', err);
+      res.status(500).json({ error: 'Erro ao atualizar o registro' });
+      return;
+    }
+    res.json({ message: 'Registro atualizado com sucesso' });
+  });
+});
+
+
   
 
 module.exports = router;
