f4c1838a9f507ecc8b9ea10e4c1a56ad20b752fd
Pollyana Valverde <valverdepolly@gmail.com>
2024-06-06 13:42:55 -0300

    sidebar em andamento

--- frontend/src/Pages/Home.js
+++ frontend/src/Pages/Home.js
@@ -5,11 +5,11 @@ import Col from 'react-bootstrap/Col';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 
-import AcessoRapidoHome from "../Components/AcessoRapidoHome";
-import ChartClienteHome from "../Components/ChartClienteHome";
-import ChartVendasHome from "../Components/ChartVendasHome";
-import ContaPagarHome from "../Components/ContaPagarHome";
-import ContaReceberHome from "../Components/ContaReceberHome";
+// import AcessoRapidoHome from "../Components/AcessoRapidoHome";
+// import ChartClienteHome from "../Components/ChartClienteHome";
+// import ChartVendasHome from "../Components/ChartVendasHome";
+// import ContaPagarHome from "../Components/ContaPagarHome";
+// import ContaReceberHome from "../Components/ContaReceberHome";
 
 import { useAuth } from '../provider/AuthProvider';
 
@@ -24,16 +24,17 @@ function Home() {
 <h1>Bem-vindo(a) {token}!</h1>
       <Row>
         <Col >
-          <AcessoRapidoHome />
-          <ChartClienteHome />
+
+          {/* <AcessoRapidoHome />
+          <ChartClienteHome /> */}
         </Col>
-        <Col >
+        {/* <Col >
           <ChartVendasHome />
           <Col lg={12} className="d-flex wrapTablesHome">
             <ContaPagarHome />
             <ContaReceberHome />
           </Col>
-        </Col>
+        </Col> */}
       </Row>
 
     </Container>
